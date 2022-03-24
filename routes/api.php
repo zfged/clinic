5e51fef72e086b31ebb23b954606822bfc0c4d65
Dima Kulbit <d_kulbit@viyar.com.ua>
2022-03-24 16:25:58 +0200

    init centers

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CentersController;
 
 
 Route::post('login', [AuthController::class, 'authenticate']);
@@ -11,4 +12,5 @@ Route::get('logout', [AuthController::class, 'logout']);
 
 // Route::group(['middleware' => ['jwt.verify','role:ROLE_ADMIN']], function() {
     Route::resource('users', UserController::class);
+    Route::resource('centers', CentersController::class);
 // });
